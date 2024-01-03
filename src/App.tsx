a7ef47e6b972d8d3fb8db6fccf899b726fbacfb0
aboutBlank-dev <josecolaco1999@gmail.com>
2024-01-03 17:14:39 +0000

    chore: install particlejs and setup test on main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,83 @@
-import { useEffect, useState } from "react";
-import NavBar from "./components/navbar";
+import { useEffect, useMemo, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
+import { Container, ISourceOptions } from "@tsparticles/engine";
 
 function App() {
-  const [isTopOfPage, setIsTopOfPage] = useState(false);
-  const [selectedPage, setSelectedPage] = useState(PageType.Home);
+  const [init, setInit] = useState(false);
 
   useEffect(() => {
-    const handleScroll = () => {
-      const isTop = window.scrollY === 0;
-      if (isTop) {
-        setIsTopOfPage(true);
-        setSelectedPage(PageType.Home);
-      } else {
-        setIsTopOfPage(false);
-      }
-    };
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
 
-    document.addEventListener("scroll", handleScroll);
-    return () => {
-      document.removeEventListener("scroll", handleScroll);
-    };
-  });
+  const particlesLoaded = async (container?: Container): Promise<void> => {
+    console.log(container);
+  };
 
-  console.log("Rendering App");
-  return (
-    <div></div>
-    // <NavBar
-    //   isTopOfPage={isTopOfPage}
-    //   selectedPage={selectedPage}
-    //   setSelectedPage={() => null}
-    // />
+  const options: ISourceOptions = useMemo(
+    () => ({
+      background: {
+        color: {
+          value: "#000000",
+        },
+      },
+      fpsLimit: 120,
+      particles: {
+        color: {
+          value: "#ffffff",
+        },
+        links: {
+          color: "#ffffff",
+          distance: 150,
+          enable: true,
+          opacity: 0.5,
+          width: 1,
+        },
+        move: {
+          direction: "none",
+          enable: true,
+          outModes: {
+            default: "bounce",
+          },
+          speed: 0.1,
+        },
+        number: {
+          density: {
+            enable: true,
+          },
+          value: 80,
+        },
+        opacity: {
+          value: 1,
+        },
+        shape: {
+          type: "square",
+        },
+        size: {
+          value: { min: 3, max: 5 },
+        },
+      },
+      detectRetina: true,
+    }),
+    []
   );
+
+  if (init) {
+    return (
+      <Particles
+        id='tsparticles'
+        particlesLoaded={particlesLoaded}
+        options={options}
+        className='fixed top-0 left-0 w-full h-full z-0'
+      />
+    );
+  }
+
+  return <></>;
 }
 
 export default App;
