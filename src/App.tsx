d16c59bebb4b49a055a4f410c1ebc6debca261f6
aboutBlank-dev <josecolaco1999@gmail.com>
2024-01-03 16:54:29 +0000

    added navBar skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,40 @@
+import { useEffect, useState } from "react";
+import NavBar from "./components/navbar";
+
 function App() {
-  return <div className=' bg-red-500'>Test</div>;
+  const [isTopOfPage, setIsTopOfPage] = useState(false);
+  const [selectedPage, setSelectedPage] = useState(PageType.Home);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const isTop = window.scrollY === 0;
+      if (isTop) {
+        setIsTopOfPage(true);
+        setSelectedPage(PageType.Home);
+      } else {
+        setIsTopOfPage(false);
+      }
+    };
+
+    document.addEventListener("scroll", handleScroll);
+    return () => {
+      document.removeEventListener("scroll", handleScroll);
+    };
+  });
+
+  console.log("Rendering App");
+  return (
+    <div></div>
+    // <NavBar
+    //   isTopOfPage={isTopOfPage}
+    //   selectedPage={selectedPage}
+    //   setSelectedPage={() => null}
+    // />
+  );
 }
 
 export default App;
+
+export enum PageType {
+  Home,
+}
---
+++ src/components/navbar.tsx
@@ -0,0 +1,17 @@
+import { PageType } from "../App";
+
+type Props = {
+  isTopOfPage: boolean;
+  selectedPage: PageType;
+  setSelectedPage: (page: string) => void;
+};
+
+function NavBar({}: Props) {
+  return (
+    <nav>
+      <div className='bg-slate-500 fixed top-0 w-full'> Test </div>
+    </nav>
+  );
+}
+
+export default NavBar;
