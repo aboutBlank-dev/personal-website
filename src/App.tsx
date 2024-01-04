42e134f0f5c7fe3ac8b00cfcc1bb7ce492b8dfbb
aboutBlank-dev <josecolaco1999@gmail.com>
2024-01-04 18:47:09 +0000

    fix: issue with trying to use useMemo outside of a react component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { useEffect, useMemo, useState } from "react";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
 import { loadSlim } from "@tsparticles/slim";
-import { Container, ISourceOptions } from "@tsparticles/engine";
+import { backgroundSourceOptions } from "./particles/sourceOptions/backgroundSourceOptions";
 
 function App() {
   const [init, setInit] = useState(false);
-
+  //Load particles.js. This should only run once per lifetime
   useEffect(() => {
     initParticlesEngine(async (engine) => {
       await loadSlim(engine);
@@ -14,66 +14,17 @@ function App() {
     });
   }, []);
 
-  const particlesLoaded = async (container?: Container): Promise<void> => {
-    console.log(container);
-  };
-
-  const options: ISourceOptions = useMemo(
-    () => ({
-      background: {
-        color: {
-          value: "#000000",
-        },
-      },
-      fpsLimit: 120,
-      particles: {
-        color: {
-          value: "#ffffff",
-        },
-        links: {
-          color: "#ffffff",
-          distance: 150,
-          enable: true,
-          opacity: 0.5,
-          width: 1,
-        },
-        move: {
-          direction: "none",
-          enable: true,
-          outModes: {
-            default: "bounce",
-          },
-          speed: 0.1,
-        },
-        number: {
-          density: {
-            enable: true,
-          },
-          value: 80,
-        },
-        opacity: {
-          value: 1,
-        },
-        shape: {
-          type: "square",
-        },
-        size: {
-          value: { min: 3, max: 5 },
-        },
-      },
-      detectRetina: true,
-    }),
-    []
-  );
-
+  const backgroundOptions = useMemo(() => backgroundSourceOptions, []);
   if (init) {
     return (
-      <Particles
-        id='tsparticles'
-        particlesLoaded={particlesLoaded}
-        options={options}
-        className='fixed top-0 left-0 w-full h-full z-0'
-      />
+      <div>
+        {/* Background of the Entire App */}
+        <Particles
+          id='backgroundParticles'
+          options={backgroundOptions}
+          className='fixed z-[-10]'
+        />
+      </div>
     );
   }
 
