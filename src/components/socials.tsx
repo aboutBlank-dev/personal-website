54b292936860bb97dbe57abb988643fe962d5f36
aboutBlank-dev <josecolaco1999@gmail.com>
2024-07-25 15:07:35 +0100

    stylized socials section and allow user to copy email. installed react-tooltip

--- src/components/socials.tsx
+++ src/components/socials.tsx
@@ -3,36 +3,58 @@
 import GithubIcon from "../assets/svg/github_icon.svg?react";
 import LinkedInIcon from "../assets/svg/linkedin_icon.svg?react";
 import EmailIcon from "../assets/svg/email_icon.svg?react";
+import { Tooltip } from "react-tooltip";
 
 export const Socials = () => {
   return (
     <div className='flex flex-col gap-2'>
       <ul className='flex justify-center lg:justify-normal mt-8 gap-4 items-center'>
-        <li className=''>
+        <li className='group'>
           <a
             href='https://github.com/aboutBlank-dev'
             target='_blank'
             rel='noreferrer'
+            className='group-hover:scale-110 transition-transform duration-300'
           >
-            <GithubIcon className='w-10 h-10' fill='white' />
+            <GithubIcon
+              className='w-8 h-8 group-hover:scale-125 transition-transform duration-100'
+              fill='white'
+            />
           </a>
         </li>
-        <li className=''>
+        <li className='group'>
           <a
             href='https://www.linkedin.com/in/jose-colaco-dev/'
             target='_blank'
             rel='noreferrer'
           >
-            <LinkedInIcon className='w-10 h-10' fill='white' />
+            <LinkedInIcon
+              className='w-8 h-8 group-hover:scale-125 transition-transform duration-100'
+              fill='white'
+            />
           </a>
         </li>
-        <li className=''>
+        <li className='group'>
           <a href='mailto:josecolaco1999@gmail.com' target='_top'>
-            <EmailIcon className='w-10 h-10' fill='white' />
+            <EmailIcon
+              className='w-8 h-8 group-hover:scale-125 transition-transform duration-100'
+              fill='white'
+            />
           </a>
         </li>
       </ul>
-      <p className='lg:text-left text-center'>josecolaco1999@gmail.com</p>
+      <Tooltip id='email-copy-tooltip' offset={-70} noArrow={true} />
+      <p
+        data-tooltip-id='email-copy-tooltip'
+        data-tooltip-content='Copy to clipboard'
+        className='lg:text-left text-center cursor-pointer'
+        onClick={() =>
+          navigator.clipboard.writeText("josecolaco1999@gmail.com")
+        }
+      >
+        josecolaco1999@gmail.com
+      </p>
+      {}
     </div>
   );
 };
